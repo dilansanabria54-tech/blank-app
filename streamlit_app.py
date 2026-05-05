133d5cc9f83f1b68bb359e016b82525e618ad775
dilansanabria54-tech <dilansanabria54@gmail.com>
2026-05-05 18:51:35 +0000

    confirmar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Nacimientos en Colombia en los Últimos 10 años, segun DANE")
+
+data = {
+    "Año": [2016,2017,2018,2019,2020,2021,2022,2023,2024,2025],
+    "Nacimientos": [647521,656704,649115,642660,629402,616914,573625,515549,453901,433678]
+}
+
+df = pd.DataFrame(data)
+df = df.set_index("Año")
+
+st.line_chart(df)
